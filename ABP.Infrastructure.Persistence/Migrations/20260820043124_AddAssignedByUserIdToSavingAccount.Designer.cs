0b8180e1b58996d038549bb44ba7d2ab135b2246
MarielyRoa <mariely6@gmail.com>
2026-08-20 00:46:29 -0400

    ultimos detalles

--- ABP.Infrastructure.Persistence/Migrations/20260820043124_AddAssignedByUserIdToSavingAccount.Designer.cs
+++ ABP.Infrastructure.Persistence/Migrations/20260820043124_AddAssignedByUserIdToSavingAccount.Designer.cs
@@ -4,6 +4,7 @@ using ABP.Infrastructure.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ABP.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ArtemisBankingAppContext))]
-    partial class ArtemisBankingAppContextModelSnapshot : ModelSnapshot
+    [Migration("20260820043124_AddAssignedByUserIdToSavingAccount")]
+    partial class AddAssignedByUserIdToSavingAccount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ABP.Infrastructure.Persistence/Migrations/20260820043124_AddAssignedByUserIdToSavingAccount.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ABP.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAssignedByUserIdToSavingAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssignedByUserId",
+                table: "SavingAccounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClientPaymentStatus",
+                table: "Loans",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PaidDate",
+                table: "LoanInstallments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpirationDate",
+                table: "CreditCards",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignedByUserId",
+                table: "SavingAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "PaidDate",
+                table: "LoanInstallments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientPaymentStatus",
+                table: "Loans",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExpirationDate",
+                table: "CreditCards",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
